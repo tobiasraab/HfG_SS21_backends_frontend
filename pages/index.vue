9a8b13faa31b11372e533fb9b0231284ff82e660
tobiasraab <64197026+tobiasraab@users.noreply.github.com>
2021-12-09 13:02:25 +0100

    add todo

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,10 @@
+<!--TODO:
+  - verschiedene Ansichten auf Statistik (Aufbauend/reset am monatsanfang)
+  - Mülleimer oder Haken
+  - Beachten: verschiedene Müllsorten (rest/bio/playstik/papier)
+  - Einheit zum Messen von Müll (Volumen bzw. Anzahl Säcke, Gewicht)
+-->
+
 <template>
   <div>
     <p>fancy datenvisualisierung</p>
