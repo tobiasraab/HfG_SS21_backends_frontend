cfa284a3c8ef42a587e093218dcecac306a9fdcf
tobiasraab <64197026+tobiasraab@users.noreply.github.com>
2022-01-03 21:31:35 +0100

    Create Api Call

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <!--TODO:
-  - verschiedene Ansichten auf Statistik (Aufbauend/reset am monatsanfang)
+  - verschiedene Ansichten auf Statistik (Aufbauend/reset am Monatsanfang)
   - Mülleimer oder Haken
-  - Beachten: verschiedene Müllsorten (rest/bio/playstik/papier)
+  - Beachten: verschiedene Müllsorten (rest/bio/Plastik/papier)
   - Einheit zum Messen von Müll (Volumen bzw. Anzahl Säcke, Gewicht)
 
   <dropdown
@@ -27,13 +27,13 @@
     <dropdown
       class="col-start-1 col-end-3 row-start-1 row-end-2"
       :label="'Jahr'"
-      :options="[2020,2021]"
+      :options="[2019, 2020, 2021, 2022]"
       :mutation="'updateGraphYear'"
     />
     <dropdown
       class="col-start-3 col-end-5 row-start-1 row-end-2"
       :label="'Monat'"
-      :options="['Januar', 'Februar', 'März']"
+      :options="['Jahr', 'Januar', 'Februar', 'März', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember']"
       :mutation="'updateGraphMonth'"
     />
     <checkbox
@@ -62,10 +62,12 @@
       :options="$store.state.graph.limit"
       :mutation="'updateGraphCheckedLimit'"
     />
+    {{ data }}
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import dropdown from '~/components/templates/dropdown.vue'
 import checkbox from '~/components/templates/checkbox.vue'
 import switcher from '~/components/templates/switcher.vue'
@@ -73,7 +75,7 @@ import bar from '~/components/templates/bar.vue'
 import graph from '~/components/templates/graph.vue'
 
 export default {
-  name: 'Start',
+  name: 'Index',
   components: {
     dropdown,
     checkbox,
@@ -81,7 +83,89 @@ export default {
     bar,
     graph
   },
-  layout: 'main-layout'
+  layout: 'main-layout',
+  data () {
+    return {
+      data: undefined
+    }
+  },
+  computed: {
+    ...mapState(['graph'])
+  },
+  watch: {
+    graph: {
+      deep: true,
+      handler () {
+        this.apiCall()
+      }
+    }
+  },
+  mounted () {
+    this.apiCall()
+  },
+  methods: {
+    apiCall () {
+      let uri
+      let month
+      const YEAR = this.$store.state.graph.year
+      switch (this.$store.state.graph.month) {
+        case 'Jahr': // Jahresansicht
+          month = 0
+          break
+        case 'Januar':
+          month = 1
+          break
+        case 'Februar':
+          month = 2
+          break
+        case 'März':
+          month = 3
+          break
+        case 'April':
+          month = 4
+          break
+        case 'Mai':
+          month = 5
+          break
+        case 'Juni':
+          month = 6
+          break
+        case 'Juli':
+          month = 7
+          break
+        case 'August':
+          month = 8
+          break
+        case 'September':
+          month = 9
+          break
+        case 'Oktober':
+          month = 10
+          break
+        case 'November':
+          month = 11
+          break
+        case 'Dezember':
+          month = 12
+          break
+      }
+      if (month === 0) {
+        uri = 'http://159.122.174.54:31312/data?year=' + YEAR.toString()
+      } else {
+        uri = 'http://159.122.174.54:31312/data?year=' + YEAR.toString() + '&month=' + month.toString()
+      }
+
+      this.$axios.$get(uri)
+        .then((res) => {
+          console.log('SUCCESS_APICALL: ', uri)
+          console.log('RESPONSE_APICALL: ', res)
+          this.$store.commit('updateData', res.data)
+        })
+        .catch((err) => {
+          console.log('ERROR_APICALL: ', err)
+        })
+    }
+  }
 }
 </script>
 
